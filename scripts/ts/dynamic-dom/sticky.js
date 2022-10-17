3e5f78e94da234fc297a82dad78ff394e1cfa027
Hginsberg01 <hginsberg@wustl.edu>
2022-10-17 12:20:22 -0500

    sticky nav bar, removed some index.html info

---
+++ scripts/ts/dynamic-dom/sticky.js
@@ -0,0 +1,22 @@
+export function setupSticky() {
+    console.log('in sticky');
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() {setupSticky()};
+
+    // Get the header
+    var header = document.getElementById('main-menu-container');
+    console.log('sticky.js');
+    console.log(header);
+
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop;
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function setupSticky() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+    }
+}
